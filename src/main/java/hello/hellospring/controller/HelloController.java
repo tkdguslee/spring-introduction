9e18312ff2c8ce4918cd5bf2005a4da732785831
Sanghyun Lee <tkdguslee92@gmail.com>
2023-05-07 00:30:15 +0900

    API 방식을 공부했습니다.

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,4 +21,30 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
-}
+
+    @GetMapping("hello-string")
+    @ResponseBody // 바디 부분에 내가 return "hello " + name;을 직접 넣어주겠다 라는 뜻.
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+            Hello hello = new Hello();
+            hello.setName(name);
+            return hello;
+        }
+
+        static class Hello {
+            private String name;
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+    }
